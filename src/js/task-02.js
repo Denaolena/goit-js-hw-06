4b060589b1c7233ad00937b163fb109ae6e51db2
olena <denaolena@gmail.com>
2023-03-16 20:47:53 +0200

    виконує task-01, 02

---
+++ src/js/task-02.js
@@ -0,0 +1,14 @@
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+// const listEl = document.querySelector('#ingredients');
+// console.log(listEl);
+
+const listEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  return item;
+});
+listEl.append(...elements);
